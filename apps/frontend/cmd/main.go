8f9c8b233ce353d1bb8becb8ede0eca777296638
Taite Nazifi <taitenazifi@gmail.com>
2025-09-18 09:58:18 -0700

    fix(frontend): naming

--- apps/frontend/cmd/main.go
+++ apps/frontend/cmd/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"log"
 
-	"github.com/Taiterbase/vtrips/apps/frontend/internal/server"
+	"github.com/Taiterbase/vtrips/apps/frontend/internal/api"
 )
 
 func main() {
 	ctx := context.Background()
-	srv := server.New()
+	srv := api.New()
 	if err := srv.Start(ctx); err != nil {
 		log.Fatalf("server stopped with error: %v", err)
 	}
